4bcdb7dca002dccbf8e7ac6abea80fc4559325c3
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-09-11 13:26:20 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+//Model
+let pictures = [
+    "grot.jpg",
+    "joker.jpg",
+    "pic2.jpg",
+    "pic3.jpg",
+    "pic4.jpg",
+    "pic5.jpg",
+
+];
+
+let currentIndex = 0;
+
+//View
+updateView();
+function updateView() {
+    document.getElementById('app').innerHTML = /*HTML*/ `
+    <nav class="menu">
+    <ul>
+        <li><a href="#">MovieDB</a></li>
+        <li><a href="#">MOVIE</a></li>
+        <li><a href="#">SERIES</a></li>
+        <li class="push"><input type="text" placeholder="Search"></li>
+    </ul>
+</nav>
+
+<main>
+    <article>
+        <div class="article-headImage article-section">
+            <img id="headImage" src="/ironMan.jpg" alt="picture of the Iron Man suite">
+        </div>
+        <div class="article-imageSlider article-section">
+            <div id="imageSlide">
+                <button id="pointersLeft" onclick="prevSlide()">ᐸ</button>
+                <img src="${pictures[(currentIndex + pictures.length - 1) % pictures.length]}" alt="picture of a movie">
+                <img src="${pictures[currentIndex]}" alt="picture of a movie">
+                <img src="${pictures[(currentIndex + 1) % pictures.length]}" alt="picture of a movie">
+                <img src="${pictures[(currentIndex + 2) % pictures.length]}" alt="picture of a movie">
+                <button id="pointersRight" onclick="nextSlide()">ᐳ</button>
+            </div>
+        </div>
+    </article>
+
+</main>
+    `;
+}
+
+
+
+//Controller
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % pictures.length;
+    updateView();
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + pictures.length) % pictures.length;
+    updateView();
+}
